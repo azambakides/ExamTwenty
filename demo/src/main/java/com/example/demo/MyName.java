2ae2c26055b28a2684277ecb385af39567a7be98
Andraea Zambakides <azambakides@gmail.com>
2021-07-23 12:12:27 +1000

    Exam submission of Demo

---
+++ demo/src/main/java/com/example/demo/MyName.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class MyName {
+
+    String name;
+
+    public MyName(String name) {
+        this.name = name;
+    }
+
+	public MyName(long incrementAndGet, String format) {
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ demo/src/test/java/com/example/demo/GreetingControllerTest.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class GreetingControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	 @Test
+	 void test() throws Exception {
+	  mvc.perform(MockMvcRequestBuilders.get("/greeting").accept(MediaType.APPLICATION_JSON))
+	    .andExpect(status().isOk())
+	    .andExpect(content().json("{\"id\": 1, \"content\": \"Hello, World!\"}"));
+	  mvc.perform(MockMvcRequestBuilders.get("/greeting").accept(MediaType.APPLICATION_JSON))
+	    .andExpect(status().isOk())
+	    .andExpect(content().json("{\"id\": 2, \"content\": \"Hello, World!\"}"));
+	 }
+	 @Test
+	 void testWithParameter() throws Exception {
+	  mvc.perform(MockMvcRequestBuilders.get("/greeting").param("name", "Testerson").accept(MediaType.APPLICATION_JSON))
+	    .andExpect(status().isOk())
+	    .andExpect(content().json("{\"content\": \"Hello, Testerson!\"}"));
+	 }
+}
